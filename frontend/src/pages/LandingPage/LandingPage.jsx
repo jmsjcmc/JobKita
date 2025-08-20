a2fdd98bd75f07db6a070c2e539f00ab17765df1
jmsjcmc <jecemecojamestabilog@gmail.com>
2025-08-20 16:53:13 +0800

    add Feature component and update LandingPage layout

--- frontend/src/pages/LandingPage/LandingPage.jsx
+++ frontend/src/pages/LandingPage/LandingPage.jsx
@@ -1,12 +1,14 @@
+import Feature from "./components/Feature";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 
 
 export default function LandingPage() {
   return (
-    <div>
+    <div className="min-h-screen mb-[100vh]">
         <Header />
         <Hero />
+        <Feature />
     </div>
   )
 }

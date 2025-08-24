b280d557e64478d3672c44972d2ff432a88c92d8
jmsjcmc <jecemecojamestabilog@gmail.com>
2025-08-24 11:39:24 +0800

    feat: implement user registration in auth controller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -7,7 +7,24 @@ const generateToken = (id) => {
 
 exports.register = async (req, res) => {
     try {
-        
+        const {name, email, password, avatar, role} = req.body;
+        const userExists = await User.findOne({email});
+        if (userExists) return res.status(400).json({message: 'User already exist'});
+        const user = await User.create({name, email, password, role, avatar});
+
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            avatar: user.avatar,
+            role: user.role,
+            token: generateToken(user._id),
+            companyName: user.companyName || '',
+            companyDescription: user.companyDescription || '',
+            companyLogo: user.companyLogo || '',
+            resume: user.resume || ''
+
+        })
     } catch (error) {
         res.status(500).json({message: error.message});
     }

6ecd0d2a3e6b62250b52c1f1d8e866622c30cbe0
jmsjcmc <jecemecojamestabilog@gmail.com>
2025-08-31 18:21:34 +0800

    feat: add empty div for layout adjustment in JobPostingForm

--- frontend/src/pages/Employer/JobPostingForm.jsx
+++ frontend/src/pages/Employer/JobPostingForm.jsx
@@ -98,6 +98,8 @@ export default function JobPostingForm() {
                   </div>
                 </div>
               </div>
+
+              <div></div>
             </div>
           </div>
         </div>

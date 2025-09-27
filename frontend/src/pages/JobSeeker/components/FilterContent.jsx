892a8e3a3c109fa9223360988f7156e3e77c2aa1
jmsjcmc <jecemecojamestabilog@gmail.com>
2025-09-27 11:09:53 +0800

    feat(filtercontent): add Job Section and salary range slider

--- frontend/src/pages/JobSeeker/components/FilterContent.jsx
+++ frontend/src/pages/JobSeeker/components/FilterContent.jsx
@@ -1,29 +1,66 @@
-import { ChevronDown, ChevronUp } from 'lucide-react';
-import React from 'react'
+import { ChevronDown, ChevronUp } from "lucide-react";
+import React from "react";
+import { JOB_TYPES } from "../../../utils/data";
+import SalaryRangeSlider from "../../../components/input/SalaryRangeSlider";
 
 export default function FilterContent({
-    toggleSection,
-    clearAllFilters,
-    expandedSections,
-    filters,
-    handleFilterChange
+  toggleSection,
+  clearAllFilters,
+  expandedSections,
+  filters,
+  handleFilterChange,
 }) {
-    const FilterSection = ({
-        title, children, isExpanded, onToggle
-    }) => (
-        <div className="border-b border-gray-200 pb-4 mb-4 last:border-b-0">
-            <button onClick={onToggle} className="flex items-center justify-between w-full text-left font-semibold text-gray-900 mb-3 hover:text-blue-600 transition-colors">
-                {title}
-                {isExpanded ? (
-                    <ChevronUp className='w-4 h-4'/>
-                ) : (
-                    <ChevronDown className='w-4 h-4'/>
-                )}
-            </button>
-            {isExpanded && children}
-        </div>
-    );
+  const FilterSection = ({ title, children, isExpanded, onToggle }) => (
+    <div className="border-b border-gray-200 pb-4 mb-4 last:border-b-0">
+      <button
+        onClick={onToggle}
+        className="flex items-center justify-between w-full text-left font-semibold text-gray-900 mb-3 hover:text-blue-600 transition-colors"
+      >
+        {title}
+        {isExpanded ? (
+          <ChevronUp className="w-4 h-4" />
+        ) : (
+          <ChevronDown className="w-4 h-4" />
+        )}
+      </button>
+      {isExpanded && children}
+    </div>
+  );
   return (
-    <div>FilterContent</div>
-  )
+    <>
+      <div className="">
+        <button onClick={clearAllFilters} className="">
+          Clear All
+        </button>
+      </div>
+      <FilterSection
+        title={"Job Type"}
+        isExpanded={expandedSections?.jobType}
+        onToggle={() => toggleSection("jobType")}
+      >
+        <div className="">
+        {JOB_TYPES.map((type) => {
+          <label key={type.value} className="">
+            <input
+              type="checkbox"
+              className=""
+              checked={filters?.type === type.value}
+              onChange={(e) =>
+                handleFilterChange("type", e.target.checked ? type.value : "")
+              }
+            />
+            <span className="">{type.value}</span>
+          </label>;
+        })}
+      </div>
+      </FilterSection>
+      
+      <FilterSection 
+      title={'Salary Range'}
+      isExpanded={expandedSections.salary}
+      onToggle={() => toggleSection('salary')}>
+        <SalaryRangeSlider />
+      </FilterSection>
+    </>
+  );
 }
